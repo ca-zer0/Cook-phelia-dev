0772b554919da6d12a8cb7a26484b5e30b57b7d7
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-30 17:04:29 +0900

    userモデル作成、マイグレーションバリデーション記述

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+         validates :password, format: { with: VALID_PASSWORD_REGEX, message: "パスワードには半角英字と半角数字の両方を含めて設定してください"}
+
+         validates :name, presence: true
+end
