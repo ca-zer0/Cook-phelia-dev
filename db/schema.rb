ed8c43e23a4666229beb485a1e666546ca2c224f
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-02 22:29:35 +0900

    recipeモデルマイグレーション記述、マイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_30_055019) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_02_131449) do
+  create_table "recipes", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "category_id", null: false
+    t.integer "kondate_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
