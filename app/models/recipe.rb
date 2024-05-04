8c9afafbcde5ee7351f8ed2879db21d69c18fc3f
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-04 15:44:13 +0900

    recipeモデルアクティブハッシュアソシエーション記述修正

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   with_options presence: true do
     validates :name
     validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
@@ -6,11 +7,12 @@ class Recipe < ApplicationRecord
     validates :image
   end
 
-    has_many :list_recipes
-    has_many :recipe_foods
-    has_many :foods, through: :recipe_foods
-    has_many :lists, through: :list_recipes
+    # has_many :list_recipes
+    # has_many :recipe_foods
+    # has_many :foods, through: :recipe_foods
+    # has_many :lists, through: :list_recipes
+    has_one_attached :image
     belongs_to :category
     belongs_to :kondate
-    has_one_attached :image
+
 end
