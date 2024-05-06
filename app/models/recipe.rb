a7b19a336d0e7960f197042479116826a4a570bb
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-06 21:29:41 +0900

    削除

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,12 +1,5 @@
 class Recipe < ApplicationRecord
 
-  with_options presence: true do
-    validates :name
-    validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
-    validates :kondate_id, numericality: { other_than: 1, message: "can't be blank"} 
-    validates :image
-  end
-  
   belongs_to :category
   belongs_to :kondate
   has_many :foods
