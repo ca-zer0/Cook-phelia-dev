49c92b094048c268eb4f36066bab6af62701138a
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-06 21:27:09 +0900

    一旦foodモデル簡略化、アソシエーション変更

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,12 +1,3 @@
 class Food < ApplicationRecord
-  with_options presence: true do
-    validates :people
-    validates :name
-    validates :amount
-  end
-
-  has_many :list_foods
-  has_many :recipe_foods
-  has_many :recipes, through: :recipe_foods
-  has_many :lists, through: :list_foods
+  belongs_to :recipe
 end
