a20117861e0605b7bf163b9f044522f102ea9346
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-08 18:20:36 +0900

    リストから削除

--- app/javascript/addlist.js
+++ app/javascript/addlist.js
@@ -7,19 +7,60 @@ $(document).ready(function(){
 
   $("[id^='add-to-list-']").on("click", function() {
     var recipeID = this.id.replace('add-to-list-', '');
+
+    if ($(this).text() == "リストに追加") {
+      $(this).text("リストから削除");
+    } else {
+      $(this).text("リストに追加");
+    }
+
     $.ajax({
-      url: "/lists",
-      method: "POST",
-      data: {
-          list: {
-            recipe_id: recipeID
-          }
-      },
-      success: function(response) {
-          console.log(response);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-          console.log(jqXHR, textStatus, errorThrown);
+      url: "/lists/check/" + recipeID,
+      method: "GET",
+      success: function(response){
+      var is_added = response.is_added;
+        var url, method, requestData;
+        if (is_added) {
+          url = "/lists/" + recipeID; // 削除用のエンドポイント
+          method = "DELETE";
+        } else {
+          url = "/lists";             // 追加用のエンドポイント
+          method = "POST";
+          requestData = {
+            list: {
+              recipe_id: recipeID
+            }
+          };
+        }
+
+        if (requestData) { //POSTの場合のみdataを送信します
+          $.ajax({
+            url: url,
+            method: method,
+            data: requestData,
+            success: function() {
+              //アイテムが正常に追加された後の処理をここに書く
+              console.log("Item added successfully");
+            },
+            error: function() {
+              //エラーコールバック
+              console.log("Something went wrong while adding the item");
+            }
+          });
+        } else { //DELETEの場合はデータを送信しません
+          $.ajax({
+            url: url,
+            method: method,
+            success: function() {
+              //アイテムが正常に削除された後の処理をここに書く
+              console.log("Item removed successfully");
+            },
+            error: function() {
+              //エラーコールバック
+              console.log("Something went wrong while removing the item");
+            }
+          });
+        };
       }
     });
   });
